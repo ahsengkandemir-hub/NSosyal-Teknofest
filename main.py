bb3cd951ad062e55a3aa5c7884a7e3aefc568597
ahsengkandemir-hub <ahsengkandemir@gmail.com>
2026-08-21 19:21:23 +0300

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def durum():
+  return render_template('deneme.html')
+
+@app.route('data.json')
+def data_json():
+  with open('data/data.json', r, encode:'utf-8') as dosya:
+    data = json.load(dosya)
+  return jsonify(data)
+
+if __name__ == __main__:
+  app.run(debug=True)
